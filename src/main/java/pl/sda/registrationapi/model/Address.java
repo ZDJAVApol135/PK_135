a00a62429905c13d8f214efbc26acb9a71168eab
Sebastian.Olszak <olipol75@gmail.com>
2023-05-20 14:48:20 +0200

    PS-8 Create Address model

---
+++ src/main/java/pl/sda/registrationapi/model/Address.java
@@ -0,0 +1,30 @@
+package pl.sda.registrationapi.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Builder
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 55)
+    private String city;
+
+    @Column(nullable = false, length = 55)
+    private String street;
+
+    @Column(nullable = false, length = 10)
+    private String postcode;
+}
